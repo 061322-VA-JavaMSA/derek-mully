cf8ab83848460b57cf7cd3094de9d0146ea06310
Derek Mully <cyberesmetv@gmail.com>
2022-06-15 00:30:41 -0400

    updated random java file

--- assignments/week1/java-compilation-activity/random.java
+++ assignments/week1/java-compilation-activity/random.java
@@ -1,6 +1,15 @@
-public class RandomNumber{
-	public static void main(String[] args){
-		System.out.println("hello!");
-}
-
+public class random{
+	
+	public static void main(String[] args) {
+		
+		System.out.println(args[0]);
+		
+		int x = Integer.parseInt(args[0]);
+		String s = args[1];
+		
+		
+		for(int i = 0; i < x; i++) {
+			System.out.println(s);
+		}
+	}
 }
