8085957d67329947e4e7b9ad50b6e95fc380dcbb
Derek Mully <cyberesmetv@gmail.com>
2022-06-24 14:15:14 -0400

    got item information to display in console

--- projects/ForklyProject0/database.sql
+++ projects/ForklyProject0/database.sql
@@ -11,7 +11,8 @@ create table if not exists users(
 drop table if exists items cascade;
 
 create table if not exists items(
-	item_id serial primary key
+	item_id serial primary key,
+	item varchar(30)
 );
 
 drop table if exists customers cascade;
@@ -40,3 +41,36 @@ drop table if exists offers cascade;
 create table if not exists offers(
 	offer_id serial primary key
 );
+
+insert into items(item)
+values ('BigFork')
+;
+
+insert into items(item)
+values ('RegularFork')
+;
+
+insert into items(item)
+values ('LittleFork')
+;
+
+alter table offers
+	add price integer
+;
+
+drop table if exists employee cascade;
+
+create table if not exists employee(
+	user_id serial primary key,
+	firstname varchar(30),
+	lastname varchar(30),
+	username varchar(30) unique not null check(length(username) > 2),
+	password varchar(30) not null
+);	
+
+alter table items
+rename column item to itemName;
+
+alter table items
+	add price integer
+;
